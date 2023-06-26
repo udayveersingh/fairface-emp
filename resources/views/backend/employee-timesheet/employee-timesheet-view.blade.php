9df2be35dd800eec02e4f9474b9ce186e94e3413
udayveersingh <uday.jbbn@gmail.com>
2023-06-26 17:54:42 +0530

    working on daterange picker

--- resources/views/backend/employee-timesheet/employee-timesheet-view.blade.php
+++ resources/views/backend/employee-timesheet/employee-timesheet-view.blade.php
@@ -313,9 +313,17 @@
     </div>
 @endsection
 @section('scripts')
+
 <script type="text/javascript" src="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.min.js"></script>
 {{-- <link rel="stylesheet" type="text/css" href="https://cdn.jsdelivr.net/npm/daterangepicker/daterangepicker.css" /> --}}
      <script>
-      $('input[name="daterange"]').daterangepicker();
+
+   $(function() {
+  $('input[name="daterange"]').daterangepicker({
+    opens: 'left'
+  }, function(start, end, label) {
+    console.log("A new date selection was made: " + start.format('YYYY-MM-DD') + ' to ' + end.format('YYYY-MM-DD'));
+  });
+});
     </script> 
 @endsection
