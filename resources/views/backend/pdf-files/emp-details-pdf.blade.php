465cba2ba52dd05fed569ff8a681ba08dd987d50
Uday Singh <uday.jbbn@gmail.com>
2023-10-05 19:25:24 +0530

    empy detail pdf style update

--- resources/views/backend/pdf-files/emp-details-pdf.blade.php
+++ resources/views/backend/pdf-files/emp-details-pdf.blade.php
@@ -8,7 +8,7 @@
     }
 
     .content {
-        padding: 20px;
+        padding: 20px 0;
         margin: 20px 0;
     }
 
@@ -20,29 +20,39 @@
     table,
     th,
     td {
-        border: 1px solid #333;
+        border: 1px solid #ddd;
         padding: 8px;
         text-align: left;
     }
+    .bg_title{background:#34444c; color:#fff; padding:8px 12px; margin-bottom:0;}
 </style>
-<div class="content">
-    <h3>Employee Basic Info</h3>
+<div class="header">
+    <table style="border:none;" bgcolor="#f8f8f8" cellpadding="0" cellspacing="0">
+        <tr>
+            <td align="middle" style="font-weight:bold; border:none;"><div style="width: 5cm;"><img src="https://i.ibb.co/1ntZs9x/logo2.png" alt="logo2" border="0" style="height:30px; vertical-align:middle;margin-right:5px; "> Fairface-Emp</div></td>
+            <td  style="border:none; background:#34444c; color:#fff; font-size:22px; font-weight:bold;">Employee Details</td>
+        </tr>
+        
+        <tr>
+            <td style="border:none; border-bottom:1px dashed #ddd;"><strong>Name:</strong> {{ $employee->firstname . ' ' . $employee->lastname }}</td>
+            <td style="border:none; border-bottom:1px dashed #ddd;"><strong>Designation:</strong> </td>
+        </tr>
+        <tr>
+            <td style="border:none;"><strong>Emp ID:</strong> {{ $employee->employee_id }}</td>
+            <td style="border:none;"><strong>Department:</strong> </td>
+        </tr>
+    </table>
+</div>
+
+<div class="content">    
     <table>
         <tr>
-            <th>Employee pic</th>
-            <td>
-                <img style="height: auto; width:80px;" src="storage/employees/{{ $employee->avatar }}" />
-            </td>
+            <td colspan="2" class="bg_title"><h3 >Basic Information</h3></td>            
         </tr>
         <tr>
             <th>Employee Id</th>
             <td>{{ $employee->employee_id }}</td>
         </tr>
-        {{-- <tr>
-            <th>Main Branch Location</th>
-            <td>{{ !empty($employee->branch->branch_code) ? $employee->branch->branch_code : '' }}
-            </td>
-        </tr> --}}
         <tr>
             <th>Name</th>
             <td>{{ $employee->firstname . ' ' . $employee->lastname }}</td>
@@ -51,10 +61,6 @@
             <th>Mobile</th>
             <td>{{ $employee->phone }}</td>
         </tr>
-        {{-- <tr>
-            <th>Alternate Phone Number</th>
-            <td>{{ $employee->alternate_phone_number }}</td>
-        </tr> --}}
         <tr>
             <th>Email</th>
             <td>{{ $employee->email }}</td>
@@ -67,10 +73,6 @@
             <th>Nationality</th>
             <td>{{ !empty($employee->country->name) ? $employee->country->name : '' }}</td>
         </tr>
-        {{-- <tr>
-            <th>National Insurance Number</th>
-            <td>{{ $employee->national_insurance_number }}</td>
-        </tr> --}}
         <tr>
             <th>Passport Number</th>
             <td>{{ $employee->passport_number }}</td>
@@ -83,16 +85,8 @@
             <th>Passport Expire Date</th>
             <td>{{ $employee->passport_expiry_date }}</td>
         </tr>
-        {{-- <tr>
-            <th>Marital Status</th>
-            <td>{{ $employee->marital_status }}</td>
-        </tr>
-        <tr>
-            <th>Record Status</th>
-            <td>{{ $employee->record_status }}</td>
-        </tr> --}}
     </table>
-    <h3>Employee Address</h3>
+    
     @foreach ($employee_addresses as $index => $address)
         @php
             if ($index > 0) {
@@ -101,6 +95,9 @@
         @endphp
         <table>
             <tr>
+                <td colspan="2" class="bg_title"><h3>Employee Address</h3></td>
+            </tr>
+            <tr>
                 <th>Address Line 1</th>
                 <td>{{ !empty($address->home_address_line_1) ? $address->home_address_line_1 : '' }}</td>
             </tr>
@@ -108,29 +105,19 @@
                 <th>Address Line 2</th>
                 <td>{{ !empty($address->home_address_line_2) ? $address->home_address_line_2 : '' }}</td>
             </tr>
-            {{-- <tr>
-                <th>Post Code</th>
-                <td>{{ !empty($address->post_code) ? $address->post_code : '' }}</td>
-            </tr> --}}
-            {{-- <tr>
-                <th>From Date</th>
-                <td>{{ !empty($address->from_date) ? date('d-m-Y', strtotime($address->from_date)) : '' }}
-                </td>
-            </tr> --}}
-            {{-- <tr>
-                <th>To Date</th>
-                <td>{{ !empty($address->to_date) ? date('d-m-Y', strtotime($address->to_date)) : '' }}</td>
-            </tr> --}}
         </table>
     @endforeach
-    <h3>Employee Visas</h3>
+    
     @foreach ($employee_visas as $index => $visa)
         @php
             if ($index > 0) {
                 break;
             }
         @endphp
-        <table>
+        <table style="border-top:20px solid #fff;">
+            <tr>
+                <td colspan="2" class="bg_title"><h3>Employee Visas</h3></td>
+            </tr>
             <tr>
                 <th>Visa Type</th>
                 <td>{{ !empty($visa->visa_types->visa_type) ? $visa->visa_types->visa_type : '' }}</td>
