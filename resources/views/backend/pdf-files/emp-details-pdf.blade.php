016f59da93d6d18cc156d02055a0efaa4ca71442
udayveersingh <uday.jbbn@gmail.com>
2023-10-09 11:57:56 +0530

    dynamic pdf details

--- resources/views/backend/pdf-files/emp-details-pdf.blade.php
+++ resources/views/backend/pdf-files/emp-details-pdf.blade.php
@@ -24,41 +24,57 @@
         padding: 8px;
         text-align: left;
     }
-    .bg_title{background:#dee6eb; color:#4c5860; padding:8px 12px; margin-bottom:0; font-size:14px;}
-    
+
+    .bg_title {
+        background: #dee6eb;
+        color: #4c5860;
+        padding: 8px 12px;
+        margin-bottom: 0;
+        font-size: 14px;
+    }
 </style>
 <div class="header">
-    <table style="border:none;"  cellpadding="0" cellspacing="0">
+    <table style="border:none;" cellpadding="0" cellspacing="0">
         <tr>
             <td colspan="2" align="middle" style="padding:0; font-weight:bold; border:none;">
-                <table style="border:none;padding-bottom:10px;"  cellpadding="0" cellspacing="0">
+                <table style="border:none;padding-bottom:10px;" cellpadding="0" cellspacing="0">
                     <tr>
-                        <td style="border:none; width:1cm; "><img src="https://i.ibb.co/1ntZs9x/logo2.png" alt="logo2" border="0" style="height:50px; vertical-align:middle;margin-right:5px; vertical-align:middle;"></td>
-                        <td style="border:none;"><span style="color:#4c5860; font-size:22px; font-weight:bold;">FAIRFACE EMP <br><span style="color:#333; font-size:14px; font-weight:normal; display:block; width:100%;">{{ ucwords(app(App\Settings\CompanySettings::class)->address ?? '') }}</span></span></td>
+                        <td style="border:none; width:1cm; "><img src="https://i.ibb.co/1ntZs9x/logo2.png"
+                                alt="logo2" border="0"
+                                style="height:50px; vertical-align:middle;margin-right:5px; vertical-align:middle;">
+                        </td>
+                        <td style="border:none;"><span style="color:#4c5860; font-size:22px; font-weight:bold;">FAIRFACE
+                                EMP <br><span
+                                    style="color:#333; font-size:14px; font-weight:normal; display:block; width:100%;">{{ ucwords(app(App\Settings\CompanySettings::class)->address ?? '') }}</span></span>
+                        </td>
                     </tr>
-                </table>                
+                </table>
             </td>
         </tr>
-        
+
         <tr>
-            <td style="border:none; border-bottom:1px dashed #ddd;"><strong>Name:</strong>{{ucfirst($employee->firstname."".$employee->lastname)}} </td>
-            <td style="border:none; border-bottom:1px dashed #ddd;"><strong>Email:</strong>{{$employee->email}}</td>
+            <td style="border:none; border-bottom:1px dashed #ddd;">
+                <strong>Name:</strong>{{ ucfirst($employee->firstname . '' . $employee->lastname) }} </td>
+            <td style="border:none; border-bottom:1px dashed #ddd;"><strong>Email:</strong>{{ $employee->email }}</td>
         </tr>
         <tr>
-            <td style="border:none;"><strong>Emp ID:</strong>{{$employee->employee_id}} </td>
-            <td style="border:none;"><strong>Mobile No:{{$employee->phone}}</strong> </td>
+            <td style="border:none;"><strong>Emp ID:</strong>{{ $employee->employee_id }} </td>
+            <td style="border:none;"><strong>Mobile No:{{ $employee->phone }}</strong> </td>
         </tr>
     </table>
 </div>
 
-<div class="content">    
+<div class="content">
     <table>
         <tr>
-            <td colspan="2" class="bg_title"><h3 >Basic Information</h3></td>            
+            <td colspan="2" class="bg_title">
+                <h3>Basic Information</h3>
+            </td>
         </tr>
         <tr>
             <th>Profile Picture</th>
-            <td><img src="https://img.freepik.com/premium-photo/man-with-orange-shirt-is-circle-with-man-orange_745528-3525.jpg" alt="profile" border="0" style="height:40px; vertical-align:middle;margin-right:5px; vertical-align:middle;"></td>
+            <td><img src="storage/employees/{{ $employee->avatar }}" alt="profile" border="0"
+                    style="height:40px; vertical-align:middle;margin-right:5px; vertical-align:middle;"></td>
         </tr>
         <tr>
             <th>Employee Id</th>
@@ -97,7 +113,7 @@
             <td>{{ $employee->passport_expiry_date }}</td>
         </tr>
     </table>
-    
+
     @foreach ($employee_addresses as $index => $address)
         @php
             if ($index > 0) {
@@ -106,7 +122,9 @@
         @endphp
         <table>
             <tr>
-                <td colspan="2" class="bg_title"><h3>Employee Address</h3></td>
+                <td colspan="2" class="bg_title">
+                    <h3>Employee Address</h3>
+                </td>
             </tr>
             <tr>
                 <th>Address Line 1</th>
@@ -118,7 +136,7 @@
             </tr>
         </table>
     @endforeach
-    
+
     @foreach ($employee_visas as $index => $visa)
         @php
             if ($index > 0) {
@@ -127,7 +145,9 @@
         @endphp
         <table style="border-top:20px solid #fff;">
             <tr>
-                <td colspan="2" class="bg_title"><h3>Employee Visas</h3></td>
+                <td colspan="2" class="bg_title">
+                    <h3>Employee Visas</h3>
+                </td>
             </tr>
             <tr>
                 <th>Visa Type</th>
@@ -157,6 +177,6 @@
                 <td>{{ !empty($visa->visa_expiry_date) ? date('d-m-Y', strtotime($visa->visa_expiry_date)) : '' }}
                 </td>
             </tr>
-        </table>    
+        </table>
     @endforeach
     <div>
