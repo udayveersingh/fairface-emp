51621bb2babc7a4a1aebe8baced8ef97e55f086a
udayveersingh <uday.jbbn@gmail.com>
2024-02-08 18:24:26 +0530

    email issue count

--- resources/views/backend/emails/email-inbox.blade.php
+++ resources/views/backend/emails/email-inbox.blade.php
@@ -177,13 +177,13 @@
 
                 <div class="list-group mail-list mt-3">
                     <a href="{{ route('user-email-inbox') }}"
-                        class="list-group-item border-0 {{ $keyword == 'inbox' ? 'active' : '' }} py-2 px-3"><i
+                        class="list-group-item border-0 {{ (isset($keyword) && ($keyword == 'inbox')) ? 'active' : '' }} py-2 px-3"><i
                             class="fas fa-download font-13 mr-2"></i>Inbox <b>({{ $total_mail_count }})</b></a>
                     <a href="{{ route('user-email-inbox', ['keyword' => 'archive']) }}"
-                        class="list-group-item border-0 py-2 px-3 {{ $keyword == 'archive' ? 'active' : '' }}"><i
+                        class="list-group-item border-0 py-2 px-3 {{ (isset($keyword) && ($keyword == 'archive')) ? 'active' : '' }}"><i
                             class="far fa-star font-13 mr-2"></i>Archive<b>({{ $archive_count }})</b></a>
                     <a href="{{ route('user-email-inbox', ['keyword' => 'sent']) }}"
-                        class="list-group-item border-0 {{ $keyword == 'sent' ? 'active' : '' }} py-2 px-3"><i
+                        class="list-group-item border-0 {{ (isset($keyword) && ($keyword == 'sent')) ? 'active' : '' }} py-2 px-3"><i
                             class="far fa-paper-plane font-13 mr-2"></i>Sent<b>({{ $sent_email_count }})</b></a>
 
                 </div>
