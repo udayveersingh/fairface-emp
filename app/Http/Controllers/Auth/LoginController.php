f43f80d0251bc69866bc1d43be7985b49b45d38e
udayveersingh <uday.jbbn@gmail.com>
2024-10-03 15:22:34 +0530

    tested on location name in logs page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -48,8 +48,9 @@ class LoginController extends Controller
             $user_log = new UserLog();
             $user_log->user_id = Auth::user()->id;
             $user_log->location_ip = $request->ip();
-            // $location = Location::get($request->ip());
+            $location = Location::get($request->ip());
             // $user_log->location_name = $location->cityName . ', ' . $location->countryCode . ' (' . $location->zipCode . ')';
+            $user_log->location_name = !empty($location->cityName) ? $location->cityName:'';
             $user_log->date_time = Carbon::now();
             $user_log->status = '1';
             $user_log->save();
