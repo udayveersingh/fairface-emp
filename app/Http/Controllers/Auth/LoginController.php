dd1ecab87a1ebd321f5820f61344c929728db255
udayveersingh <uday.jbbn@gmail.com>
2024-10-03 14:24:22 +0530

    bugs fixed in logs page

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -49,7 +49,7 @@ class LoginController extends Controller
             $user_log->user_id = Auth::user()->id;
             $user_log->location_ip = $request->ip();
             $location = Location::get($request->ip());
-            $user_log->location_name =  $location->cityName . ', ' . $location->countryCode . ' (' . $location->zipCode . ')';
+            $user_log->location_name = $location->cityName . ', ' . $location->countryCode . ' (' . $location->zipCode . ')';
             $user_log->date_time = Carbon::now();
             $user_log->status = '1';
             $user_log->save();
