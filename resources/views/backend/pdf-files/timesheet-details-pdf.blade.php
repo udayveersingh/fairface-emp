82c4763af93c984261a18ec182fe7d930520b310
udayveersingh <uday.jbbn@gmail.com>
2024-10-07 19:05:47 +0530

    bugs fixed in pdf file

--- resources/views/backend/pdf-files/timesheet-details-pdf.blade.php
+++ resources/views/backend/pdf-files/timesheet-details-pdf.blade.php
@@ -42,10 +42,10 @@
                     <table style="border:none;padding-bottom:10px;" cellpadding="0" cellspacing="0">
                         <tr>
                         <td style="border:none; width:1cm;">
-                                <img src="storage/settings/theme/{{app(App\Settings\ThemeSettings::class)->logo }}"
-                                    alt="profile" border="0"
-                                    style="height:50px; vertical-align:middle;margin-right:5px; vertical-align:middle;">
-                            </td>
+                            <img src="storage/settings/theme/{{app(App\Settings\ThemeSettings::class)->logo }}"
+                                alt="profile" border="0"
+                                style="height:50px; vertical-align:middle;margin-right:5px; vertical-align:middle;">
+                        </td>
                             <td style="border:none;"><span style="color:#4c5860; font-size:22px; font-weight:bold;">
                                     {{ ucwords(app(App\Settings\CompanySettings::class)->company_name ?? '') }} <br><span
                                         style="color:#333; font-size:14px; font-weight:normal; display:block; width:100%;">{{ ucwords(app(App\Settings\CompanySettings::class)->address ?? '') }}</span></span>
