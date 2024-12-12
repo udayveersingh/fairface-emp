1dbdd72b162ed279dbc0f73e5bfddad62ca7d613
udayveersingh <uday.jbbn@gmail.com>
2024-12-12 16:14:18 +0530

    login status

--- app/Http/Controllers/Api/TimesheetController.php
+++ app/Http/Controllers/Api/TimesheetController.php
@@ -3,6 +3,7 @@
 namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
+use App\Models\EmployeeTimesheet;
 use App\Models\TimesheetStatus;
 use Illuminate\Http\Request;
 
@@ -21,8 +22,6 @@ class TimesheetController extends Controller
     
     public function store(Request $request) 
     {
-
-        return response()->json(['success' => true, 'data' => $request->weeklyData], 201);
         $year = $request->year;
         $month = ($request->month >= 10) ? $request->month : '0' . $request->month;
         $first_date = $year . "-" . $month . "-01";
@@ -48,6 +47,13 @@ class TimesheetController extends Controller
         // ]);
         
         $timesheet_status = TimesheetStatus::where('status', TimesheetStatus::PENDING_APPROVED)->first();
+
+       // Decode the JSON string into a PHP associative array
+        $weeklyData = json_decode($request->weeklyData, true);
+
+        return $weeklyData;
+
+
         $calender_date = $request->input('calender_date');
         $calender_day = $request->input('calender_day');
         $start_time = $request->input('start_time');
@@ -57,6 +63,52 @@ class TimesheetController extends Controller
         $project_phase_id = $request->input('project_phase_id');
         $notes = $request->input('notes');
         $timesheet_id = "ISL-TM-" . Str::random(6);
+
+        foreach ($start_time as $key => $value) {
+            if (!empty($hours[$key]) && $hours[$key] == "full_day") {
+                $total_hours_worked = "8 hours";
+            } elseif (!empty($hours[$key]) && $hours[$key] == "half_day") {
+                $total_hours_worked = "4 hours";
+            } else {
+                $total_hours_worked = "";
+            }
+
+            $employee_timesheet = EmployeeTimesheet::where('employee_id', '=', $request->employee_id)->where('calender_date', '=', $calender_date[$key])->first();
+            if (empty($employee_timesheet)) {
+                $emp_timesheet = new EmployeeTimesheet();
+                $message = "Employee TimeSheet Data has been added successfully!!.";
+            } else {
+                $emp_timesheet = EmployeeTimesheet::find($employee_timesheet->id);
+                $message = "Employee TimeSheet Data has been updated successfully!!.";
+            }
+            
+            $emp_timesheet->timesheet_id = $timesheet_id;
+            $emp_timesheet->employee_id = $request->input('employee_id');
+            $emp_timesheet->supervisor_id = $request->input('supervisor_id');
+            $emp_timesheet->project_id = $project_id[$key];
+            $emp_timesheet->project_phase_id  = $project_phase_id[$key];
+            $emp_timesheet->calender_day = $calender_day[$key];
+            $emp_timesheet->calender_date = $calender_date[$key];
+            if ($value == Null && !empty($hours[$key]) && $hours[$key] == "full_day" ||  $hours[$key] == "half_day") {
+                $emp_timesheet->from_time = "9:00";
+            } else {
+                $emp_timesheet->from_time = $value;
+            }
+
+            if ($end_time[$key] == Null && !empty($hours[$key]) && $hours[$key] == "full_day") {
+                $emp_timesheet->to_time = "17:00";
+            } else if ($end_time[$key] == Null && !empty($hours[$key]) && $hours[$key] == "half_day") {
+                $emp_timesheet->to_time = "13:00";
+            } else {
+                $emp_timesheet->to_time = $end_time[$key];
+            }
+            $emp_timesheet->notes =  $notes[$key];
+            $emp_timesheet->total_hours_worked = $total_hours_worked;
+            $emp_timesheet->timesheet_status_id = $timesheet_status->id;
+            $emp_timesheet->start_date = $start_date;
+            $emp_timesheet->end_date = $end_date;
+            $emp_timesheet->save();
+        }
 
     }
 }
